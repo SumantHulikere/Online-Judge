26ebed56f209d00692878b49aae904449242b5a1
SumantHulikere <66783085+SumantHulikere@users.noreply.github.com>
2020-06-11 18:07:21 +0530

    Update 435A Queue on Bus Stop.cpp
    can this be done in this way

--- CodeForce/435A Queue on Bus Stop.cpp
+++ CodeForce/435A Queue on Bus Stop.cpp
@@ -1,19 +1,24 @@
 ﻿/* 6763649	 Jun 1, 2014 2:19:07 PM	Shark	 435A - Queue on Bus Stop	 GNU C++	Accepted	 15 ms	 0 KB */
-#include<cstdio>
-int main(){
-	int n, m;
-	while (scanf("%d%d", &n, &m) == 2){
-		int S[100], ans = 0;
-		for (int i = 0; i < n; i++)
-			scanf("%d", S + i);
-		for (int i = 0, cap = 0; i < n; cap = 0){
-			while (i < n && cap + S[i] <= m)
-				cap += S[i++];
-			ans++;
-		}
-		printf("%d\n", ans);
-	}
-	return 0;
+#include<iostream>
+
+using namespace std;
+int main()
+{
+ int n,m,a[n];
+  std::cin>>n>>m;
+  int sum=0;
+  for(int i=0;i<n;i++)
+  {
+    std::cin>>a[i];
+    sum=sum+a[i];
+  }
+  
+int j=sum/m;
+  if(sum%m==0)
+  {std::cout<<j;
+  }
+  else
+    std::cout<<++j;
 }
 /*
 
